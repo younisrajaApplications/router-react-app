51130ff437850717caac12b0ec65a24705d93466
younisrajaApplications <younisraja.applications@gmail.com>
2024-09-18 20:48:57 +0100

    Create app which navigates to multiples pages

---
+++ src/Contact.js
@@ -0,0 +1,13 @@
+// Contact.js
+import React from 'react';
+
+function Contact() {
+  return (
+    <div>
+      <h1>Contact Page</h1>
+      <p>Get in touch with us on the Contact Page.</p>
+    </div>
+  );
+}
+
+export default Contact;
